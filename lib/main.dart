ed2d2490010fda9ba0df1243270a955eb036d137
guilhermecandido <guilherme.candido96@gmail.com>
2023-01-18 11:22:10 -0300

    LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:canhoto_digital_faxon_v2/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,33 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyHomePage(title: 'Faxon'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
